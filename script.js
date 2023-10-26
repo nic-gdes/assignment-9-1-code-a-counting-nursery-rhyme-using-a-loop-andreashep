4ab59ae57e874a942b13124cac5d394635aca195
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-10-26 14:53:50 -0700

    last stanza connected to var(song)

--- script.js
+++ script.js
@@ -10,21 +10,30 @@ let word = "little monkey";
 var count = 10;
 const list = document.querySelector("#song");
 while (count > 0) {
-  var song = count + " " + word + "s" + " jumping on the bed";
+  var song =
+    count +
+    " " +
+    word +
+    "s" +
+    " jumping on the bed." +
+    " One fell down and bumped his head." +
+    " Mamma called the Dr. and the Dr. said," +
+    " No more monkeys jumping on the bed";
+  if (count === 1) {
+      song = 
+      count +
+        " " +
+        word +
+        " jumping on the bed." +
+        " she fell off and bumped her head" +
+        " Mamma called the Dr. and the Dr. said" +
+        " Put those monkeys right to bed";
+    }
   var paragraph = document.createElement("p");
   paragraph.textContent = song;
   list.appendChild(paragraph);
 
-  console.log("one fell down and bumped his head");
-  console.log("Mamma called the Dr. and the Dr. said");
-  console.log("No more monkeys jumping on the bed");
-
   count = count - 1;
 
-  if (count === 1) {
-    console.log(count + " " + word + " jumping on the bed");
-    console.log("she fell off and bumped her head");
-    console.log("Mamma called the Dr. and the Dr. said");
-    console.log("Put those monkeys right to bed");
-    };
+  
 }
