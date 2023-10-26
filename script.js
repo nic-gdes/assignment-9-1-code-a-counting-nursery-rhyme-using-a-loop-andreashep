a98177b63f6ff01543603ee58deaffc4bad0eed0
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-10-26 14:58:58 -0700

    grammer fixes

--- script.js
+++ script.js
@@ -18,22 +18,20 @@ while (count > 0) {
     " jumping on the bed." +
     " One fell down and bumped his head." +
     " Mamma called the Dr. and the Dr. said," +
-    " No more monkeys jumping on the bed";
+    "  'No more monkeys jumping on the bed'.";
   if (count === 1) {
-      song = 
+    song =
       count +
-        " " +
-        word +
-        " jumping on the bed." +
-        " she fell off and bumped her head" +
-        " Mamma called the Dr. and the Dr. said" +
-        " Put those monkeys right to bed";
-    }
+      " " +
+      word +
+      " jumping on the bed." +
+      " She fell off and bumped her head." +
+      " Mamma called the Dr. and the Dr. said," +
+      "  'Put those monkeys right to bed.'";
+  }
   var paragraph = document.createElement("p");
   paragraph.textContent = song;
   list.appendChild(paragraph);
 
   count = count - 1;
-
-  
 }
