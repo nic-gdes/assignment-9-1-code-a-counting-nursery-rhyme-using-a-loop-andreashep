3cd7de84bb597b292c1812dd7fd032a01b3841c9
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-10-26 13:39:11 -0700

    nursery rhyme branch 1, set up, counting lines work

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ One fell down and bumped his head,
 Mama called the doctor and the doctor said,
 
 No more monkeys jumping on the bed!*/
-        
+
 let word = "little monkey";
-    var count = 10;
+var count = 10;
+const list = document.querySelector("#song");
+while (count > 0) {
+  var song = count + " " + word + "s" + " jumping on the bed";
+  var paragraph = document.createElement("p");
+  paragraph.textContent = song;
+  list.appendChild(paragraph);
 
-while (count > 1) {
-    console.log(count + " "  + word + "s" + " jumping on the bed");
-    console.log("one fell down and bumped his head");
+  console.log("one fell down and bumped his head");
+  console.log("Mamma called the Dr. and the Dr. said");
+  console.log("No more monkeys jumping on the bed");
+
+  count = count - 1;
+
+  if (count === 1) {
+    console.log(count + " " + word + " jumping on the bed");
+    console.log("she fell off and bumped her head");
     console.log("Mamma called the Dr. and the Dr. said");
-    console.log("No more monkeys jumping on the bed");
-    count = count - 1;
-
-    if (count === 1) {
-        console.log(count + " " + word + " jumping on the bed");
-        console.log("she fell off and bumped her head");
-        console.log("Mamma called the Dr. and the Dr. said");
-        console.log("Put those monkeys right to bed");
-    } 
-    }
+    console.log("Put those monkeys right to bed");
+    };
+}
